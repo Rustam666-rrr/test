a1cdefedbc2a6830efe5fceabe468e03a3a030d3
unknown <fndiyanrustam@gmail.com>
2021-10-02 19:49:17 +0300

    Это наш первый комит

---
+++ 1/app.js
@@ -0,0 +1,29 @@
+//alert('RRR');
+
+obj = {
+	name:'Rustam',
+	age: 21,
+	IsMarried: false
+}
+console.log(obj.name);
+console.log(obj ['age']);
+var arr = ['orange', 'green', 'red', 'blue']
+console.log(arr[3]);
+confirm('How are you?');
+// prompt("Вам есть 18 лет", '18');
+ var answer = + prompt("Вам есть 18 лет", '18');
+ console.log(typeof (answer));
+ console.log(4 + " - object");
+
+ var incr = 10;
+ var decr = 10;
+ incr++;
+ decr--;
+ console.log(incr);
+ console.log(decr);
+
+ var isCheked = true;
+ var isClose =true;
+ console.log(isCheked && isClose);
+ console.log(isCheked && !isClose);
+  console.log(isCheked || isClose);
